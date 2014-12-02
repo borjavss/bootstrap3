e4c4b04bea14d232d37b76c37fac0bde77f1ee70
borjavss <borjavss@gmail.com>
2014-12-02 11:46:03 +0100

    Bootstrap Template

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <title>Bootstrap Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.min.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+
+    <!-- FAVICON -->
+    <!-- <link rel="icon" href="../images/favicon.ico"> -->
+  </head>
+
+  <body>
+    <h1>Hello World!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+    <script src="_/js/bootstrap.min.js"></script>
+    <script src="_/js/myscript.js"></script>
+    <!-- aditional features for IE 8 <script src="js/respond.js"></script> -->
+    <!-- I think you can activate last jquery wiht next line: -->
+    <!-- <script src="http://code.jquery.com/jquery-latest.min.js"></script> -->
+  </body>
+</html>
